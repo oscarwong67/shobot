fb9bb39cfef78ae51fb1701757ef0fa2168f1fa4
Oscar Wong <oscar@wadomp.com>
2016-06-15 20:38:48 -0600

    Added Coin Flip and Team Generator Functionality

--- README.txt
+++
@@ -1,9 +0,0 @@
-
-Danica-Bot
-A Discord Bot
-
-Danica-Bot is a bot for Discord, which allows the use of various functions. Most notably, commands include:
-
-!danica - Plays a Whale Sound
-
-Note: Config.json is not included, but there is an example. Get yourself a token!
--- danica.js
+++ danica.js
@@ -7,12 +7,12 @@ danica.on("ready", function() {
 	danica.setStatus("online", "Whale Simulator");
 });
 
-danica.on("message", function(message) {
+danica.on("message", function(message) {	
 
-	var random = Math.floor(Math.random() * 3 + 1);
-
-	if (message.content === "!danica") {
+	if (message.content.toUpperCase() === "!DANICA") {
 		danica.joinVoiceChannel(message.sender.voiceChannel, function(error, connection) {
+		var random = Math.floor(Math.random() * 3 + 1);
+
 			//change this directory, obviously
 			connection.playFile("E:\\Users\\Oscar\\Coding\\Github\\danica-bot\\audio\\whale" + random + ".wav", function(error, intent) {
 				intent.on("end", function() {
@@ -20,6 +20,38 @@ danica.on("message", function(message) {
 				});
 			});
 		});
+	}
+	else if (message.content.toUpperCase() === "!COINTOSS" || message.content.toUpperCase() === "!COINFLIP" ) {
+		var flip = Math.floor(Math.random() * 2 + 1);
+		if (flip === 1)
+			danica.reply(message, "tails!");
+		else
+			danica.reply(message, "heads!");
+	}
+	else if (message.content.toUpperCase() === "!TEAMS") {
+		var players = message.sender.voiceChannel.members;
+		var teamOne = [];
+		var teamTwo = [];
+		var teamOneSize = (players.length / 2) - 1;
+		var teamTwoPlayerOne = teamOneSize + 1;
+		var numbers = [];
+		var i = 0;
+		var x = 1;
+
+		while (numbers.length < players.length) {
+			var randomNum = Math.floor(Math.random() * (players.length) );
+			if (numbers.indexOf(randomNum) === -1) {
+				numbers.push(randomNum);				
+			}
+		}
+		while (teamOne.length < (players.length / 2)) {
+			teamOne.push(" " + players[numbers[i]].username);
+			if (players[numbers[x]] !== undefined)
+				teamTwo.push(" " + players[numbers[x]].username);
+			i += 2;
+			x += 2;
+		}
+		danica.reply(message, "Team One is: " + teamOne + " and Team Two is: " + teamTwo);
 	}
 });
 
